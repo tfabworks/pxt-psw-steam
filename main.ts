692a7c1cf660a7094bca3cfa5e52260bf0865ed0
atsushi morita <himarayayukiotoko@gmail.com>
2021-12-14 15:13:17 +0900

    update

--- main.ts
+++ main.ts
@@ -612,6 +612,7 @@ namespace psw_steam {
     //% blockId=SL1_sound_pressure block="音の大きさ(SL1)"
     //% group="SL1"
     //% weight=100
+	//% advanced=true
     export function SL1_sound_pressure(): number {
         return pins.i2cReadNumber(8, NumberFormat.UInt16BE, false)
     }
