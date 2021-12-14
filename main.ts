88f18b1b1770359d17bc1c94831e043e6332f390
atsushi morita <himarayayukiotoko@gmail.com>
2021-12-14 14:28:35 +0900

    update

--- main.ts
+++ main.ts
@@ -18,7 +18,7 @@ enum OutputNumberFormat {
     FLOAT = 1
 }
 
-//% weight=115 icon="\uf0e7" color=#d2691e block="STEAM"
+//% weight=115 icon="\uf0e7" color=#002FFF block="STEAM"
 namespace psw_steam {
 
     /**
@@ -262,5 +262,224 @@ namespace psw_steam {
     //% group="micro:bit本体"
     export function pause_sec(sec: number) {
         basic.pause(1000 * sec);
+    }
+	
+	/**
+     * TFW-DS1で距離[cm]を測定します。
+     * @param format number format, eg: OutputNumberFormat.INTEGER
+     */
+    //% blockId=uds
+    //% block="距離[cm] || %format"
+    //% group="DS1"
+    //% weight=100
+    export function getDistance(format:OutputNumberFormat = OutputNumberFormat.INTEGER):number {
+        // calculate distance -> median filter -> return value
+        let arr: number[] = [];
+        let l;
+        for(l = 0; l < 3; l++) {
+            let pulseWidth;
+            const MAX_DIST_CM = 300;
+            pins.digitalWritePin(DigitalPin.P0, 0);
+            control.waitMicros(60000);
+            pins.digitalWritePin(DigitalPin.P0, 1);
+            control.waitMicros(20);
+            pins.digitalWritePin(DigitalPin.P0, 0);
+            pulseWidth = pins.pulseIn(DigitalPin.P1, PulseValue.High);
+
+            let distance_cm;
+            if (pulseWidth == 0) {
+                distance_cm = MAX_DIST_CM;
+            }
+            else if (pulseWidth >= MAX_DIST_CM / 153 * 29 * 2 * 100) {
+                distance_cm = MAX_DIST_CM;
+            }
+            else{
+                distance_cm = pulseWidth * 153 / 29 / 2 / 100;
+            }
+
+            if ( format === OutputNumberFormat.INTEGER ){
+                distance_cm = Math.round( distance_cm );
+            }
+
+            arr.push(distance_cm);
+        }
+        arr.sort((n1, n2) => n1 - n2);
+        return arr[1];
+    }
+	
+	    const ビットパターン_ON = [1, 1, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0]
+    const ビットパターン_OFF = [1, 1, 1, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0]
+    const 繰り返し回数 = 30
+    const 輸送波パルス幅 = 600
+    const 輸送波周期 = 26
+
+    function 輸送波送信ポート設定() {
+        pins.analogWritePin(AnalogPin.P0, 輸送波パルス幅)
+        pins.analogSetPeriod(AnalogPin.P0, 輸送波周期)
+    }
+
+    /**
+     * TFW-IR2で赤外線リモコンコンセントOCR-05WをONします。
+     */
+    //% blockId=ir_on
+    //% block="赤外線ON"
+    //% group="IR2"
+    //% weight=100
+    export function IR_ON() {
+        輸送波送信ポート設定()
+
+        for (let 送信回数 = 0; 送信回数 < 繰り返し回数; 送信回数++) {
+            pins.digitalWritePin(DigitalPin.P1, ビットパターン_ON[0])
+            control.waitMicros(850)
+            pins.digitalWritePin(DigitalPin.P1, ビットパターン_ON[1])
+            control.waitMicros(850)
+            pins.digitalWritePin(DigitalPin.P1, ビットパターン_ON[2])
+            control.waitMicros(850)
+            pins.digitalWritePin(DigitalPin.P1, ビットパターン_ON[3])
+            control.waitMicros(850)
+            pins.digitalWritePin(DigitalPin.P1, ビットパターン_ON[4])
+            control.waitMicros(850)
+            pins.digitalWritePin(DigitalPin.P1, ビットパターン_ON[5])
+            control.waitMicros(850)
+            pins.digitalWritePin(DigitalPin.P1, ビットパターン_ON[6])
+            control.waitMicros(850)
+            pins.digitalWritePin(DigitalPin.P1, ビットパターン_ON[7])
+            control.waitMicros(850)
+            pins.digitalWritePin(DigitalPin.P1, ビットパターン_ON[8])
+            control.waitMicros(850)
+            pins.digitalWritePin(DigitalPin.P1, ビットパターン_ON[9])
+            control.waitMicros(850)
+            pins.digitalWritePin(DigitalPin.P1, ビットパターン_ON[10])
+            control.waitMicros(850)
+            pins.digitalWritePin(DigitalPin.P1, ビットパターン_ON[11])
+            control.waitMicros(850)
+            pins.digitalWritePin(DigitalPin.P1, ビットパターン_ON[12])
+            control.waitMicros(850)
+            pins.digitalWritePin(DigitalPin.P1, ビットパターン_ON[13])
+            control.waitMicros(850)
+            pins.digitalWritePin(DigitalPin.P1, ビットパターン_ON[14])
+            control.waitMicros(850)
+            pins.digitalWritePin(DigitalPin.P1, ビットパターン_ON[15])
+            control.waitMicros(850)
+            pins.digitalWritePin(DigitalPin.P1, ビットパターン_ON[16])
+            control.waitMicros(850)
+            pins.digitalWritePin(DigitalPin.P1, ビットパターン_ON[17])
+            control.waitMicros(850)
+            pins.digitalWritePin(DigitalPin.P1, ビットパターン_ON[18])
+            control.waitMicros(850)
+            pins.digitalWritePin(DigitalPin.P1, ビットパターン_ON[19])
+            control.waitMicros(850)
+            pins.digitalWritePin(DigitalPin.P1, ビットパターン_ON[20])
+            control.waitMicros(850)
+            pins.digitalWritePin(DigitalPin.P1, ビットパターン_ON[21])
+            control.waitMicros(850)
+            pins.digitalWritePin(DigitalPin.P1, ビットパターン_ON[22])
+            control.waitMicros(850)
+            pins.digitalWritePin(DigitalPin.P1, ビットパターン_ON[23])
+            control.waitMicros(850)
+            pins.digitalWritePin(DigitalPin.P1, ビットパターン_ON[24])
+            control.waitMicros(850)
+            pins.digitalWritePin(DigitalPin.P1, ビットパターン_ON[25])
+            control.waitMicros(850)
+            pins.digitalWritePin(DigitalPin.P1, ビットパターン_ON[26])
+            control.waitMicros(850)
+            pins.digitalWritePin(DigitalPin.P1, ビットパターン_ON[27])
+            control.waitMicros(850)
+            pins.digitalWritePin(DigitalPin.P1, ビットパターン_ON[28])
+            control.waitMicros(850)
+            pins.digitalWritePin(DigitalPin.P1, ビットパターン_ON[29])
+            control.waitMicros(850)
+            pins.digitalWritePin(DigitalPin.P1, ビットパターン_ON[30])
+            control.waitMicros(850)
+        }
+    }
+
+    /**
+     * TFW-IR2で赤外線リモコンコンセントOCR-05WをOFFします。
+     */
+    //% blockId=ir_off
+    //% block="赤外線OFF"
+    //% group="IR2"
+    //% weight=90
+    export function IR_OFF() {
+        輸送波送信ポート設定()
+
+        for (let 送信回数 = 0; 送信回数 < 繰り返し回数; 送信回数++) {
+            pins.digitalWritePin(DigitalPin.P1, ビットパターン_ON[0])
+            control.waitMicros(850)
+            pins.digitalWritePin(DigitalPin.P1, ビットパターン_OFF[1])
+            control.waitMicros(850)
+            pins.digitalWritePin(DigitalPin.P1, ビットパターン_OFF[2])
+            control.waitMicros(850)
+            pins.digitalWritePin(DigitalPin.P1, ビットパターン_OFF[3])
+            control.waitMicros(850)
+            pins.digitalWritePin(DigitalPin.P1, ビットパターン_OFF[4])
+            control.waitMicros(850)
+            pins.digitalWritePin(DigitalPin.P1, ビットパターン_OFF[5])
+            control.waitMicros(850)
+            pins.digitalWritePin(DigitalPin.P1, ビットパターン_OFF[6])
+            control.waitMicros(850)
+            pins.digitalWritePin(DigitalPin.P1, ビットパターン_OFF[7])
+            control.waitMicros(850)
+            pins.digitalWritePin(DigitalPin.P1, ビットパターン_OFF[8])
+            control.waitMicros(850)
+            pins.digitalWritePin(DigitalPin.P1, ビットパターン_OFF[9])
+            control.waitMicros(850)
+            pins.digitalWritePin(DigitalPin.P1, ビットパターン_OFF[10])
+            control.waitMicros(850)
+            pins.digitalWritePin(DigitalPin.P1, ビットパターン_OFF[11])
+            control.waitMicros(850)
+            pins.digitalWritePin(DigitalPin.P1, ビットパターン_OFF[12])
+            control.waitMicros(850)
+            pins.digitalWritePin(DigitalPin.P1, ビットパターン_OFF[13])
+            control.waitMicros(850)
+            pins.digitalWritePin(DigitalPin.P1, ビットパターン_OFF[14])
+            control.waitMicros(850)
+            pins.digitalWritePin(DigitalPin.P1, ビットパターン_OFF[15])
+            control.waitMicros(850)
+            pins.digitalWritePin(DigitalPin.P1, ビットパターン_OFF[16])
+            control.waitMicros(850)
+            pins.digitalWritePin(DigitalPin.P1, ビットパターン_OFF[17])
+            control.waitMicros(850)
+            pins.digitalWritePin(DigitalPin.P1, ビットパターン_OFF[18])
+            control.waitMicros(850)
+            pins.digitalWritePin(DigitalPin.P1, ビットパターン_OFF[19])
+            control.waitMicros(850)
+            pins.digitalWritePin(DigitalPin.P1, ビットパターン_OFF[20])
+            control.waitMicros(850)
+            pins.digitalWritePin(DigitalPin.P1, ビットパターン_OFF[21])
+            control.waitMicros(850)
+            pins.digitalWritePin(DigitalPin.P1, ビットパターン_OFF[22])
+            control.waitMicros(850)
+            pins.digitalWritePin(DigitalPin.P1, ビットパターン_OFF[23])
+            control.waitMicros(850)
+            pins.digitalWritePin(DigitalPin.P1, ビットパターン_OFF[24])
+            control.waitMicros(850)
+            pins.digitalWritePin(DigitalPin.P1, ビットパターン_OFF[25])
+            control.waitMicros(850)
+            pins.digitalWritePin(DigitalPin.P1, ビットパターン_OFF[26])
+            control.waitMicros(850)
+            pins.digitalWritePin(DigitalPin.P1, ビットパターン_OFF[27])
+            control.waitMicros(850)
+            pins.digitalWritePin(DigitalPin.P1, ビットパターン_OFF[28])
+            control.waitMicros(850)
+            pins.digitalWritePin(DigitalPin.P1, ビットパターン_OFF[29])
+            control.waitMicros(850)
+            pins.digitalWritePin(DigitalPin.P1, ビットパターン_OFF[30])
+            control.waitMicros(850)
+        }
+    }
+	
+	/**
+     * TFW-SW1で出力をコントロールします。
+     * @param duty set the duty-ratio, eg: 100
+     */
+    //% blockId=sw1_out
+    //% block="出力を%duty|\\%にする"
+    //% duty.min=0 duty.max=100
+    //% group="SW1"
+    //% weight=60
+    export function sw1_out(duty: number): void {
+        pins.analogWritePin(AnalogPin.P0, (duty / 100 * 1023));
     }
 }
