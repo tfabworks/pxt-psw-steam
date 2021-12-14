cccb581e7240965d81ea545f03a7d3ffbd98cd53
atsushi-morita <47807023+atsushi-morita@users.noreply.github.com>
2021-12-14 14:06:33 +0900

    更新 pxt.json, test.blocks, test.ts

--- test.ts
+++ test.ts
@@ -1,10 +1,7 @@
+// basic.showNumber(kkk.light_level())
 basic.forever(function () {
-    if (kkk.is_man_moving()) {
-        kkk.turn_on()
-    } else {
-        kkk.turn_off()
-    }
+	
 })
 basic.forever(function () {
-    basic.showNumber(kkk.light_level())
+	
 })
